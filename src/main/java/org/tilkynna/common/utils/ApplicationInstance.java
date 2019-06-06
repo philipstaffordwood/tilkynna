b2c50b087f762aa50569806cad34a6970b7ce9d7
melissa.palmer <melissa.palmer@gmail.com>
2019-06-06 16:53:19 +0200

    only calculation the ApplicationInstance.name() once on initialization

--- src/main/java/org/tilkynna/common/utils/ApplicationInstance.java
+++ src/main/java/org/tilkynna/common/utils/ApplicationInstance.java
@@ -9,19 +9,31 @@ package org.tilkynna.common.utils;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationInstance {
+
+    private static String name;
+
     private ApplicationInstance() {
     }
 
     public static String name() {
-        try {
-            return InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            return "Unknown";
-        }
+        return name;
+    }
 
+    /**
+     * Only calculate the ApplicationInstance name once, so that we are not needing to call `InetAddress.getLocalHost().getHostName();` each time we want ApplicationInstance.name()
+     */
+    @PostConstruct
+    private void initName() {
+        try {
+            name = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            name = "Unknown";
+        }
     }
 }
